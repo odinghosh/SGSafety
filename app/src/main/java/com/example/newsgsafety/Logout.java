4f755ed06050d9d740c495929676dd2d9ccc7db0
WillisTee <willisteetk@hotmail.com>
2021-09-10 12:32:18 +0800

    Added icons

---
+++ app/src/main/java/com/example/newsgsafety/Logout.java
@@ -0,0 +1,13 @@
+package com.example.newsgsafety;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Logout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logout);
+    }
+}
