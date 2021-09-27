8d6fb9504c39d5702befcd1dfdf6a12d6fc8291f
WillisTee <willisteetk@hotmail.com>
2021-09-27 18:18:03 +0800

    Added PanicLocation UI, MapView is yet to be implemented

--- app/src/main/java/com/example/newsgsafety/PanicLocation.java
+++ app/src/main/java/com/example/newsgsafety/PanicLocation.java
@@ -5,16 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 
-import com.google.firebase.auth.FirebaseAuth;
-
-public class MainActivity extends AppCompatActivity {
+public class PanicLocation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_panic_location);
 
     }
 
