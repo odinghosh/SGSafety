92e1a46a1672da744e3e8b8831763dd278b9bdfc
WillisTee <willisteetk@hotmail.com>
2021-09-28 16:33:41 +0800

    Added in location button, cancel panic request button

---
+++ app/src/main/java/com/example/newsgsafety/UV.java
@@ -0,0 +1,13 @@
+package com.example.newsgsafety;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UV extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uv);
+    }
+}
