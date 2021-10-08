b0f3bf688e35dbf088fc73310fecf3ee065ba679
chongyian <cyaction@gmail.com>
2021-10-08 17:51:15 +0800

    nice to meet you is this how it works i supposedly added appicon

--- app/src/main/java/com/example/newsgsafety/ConfirmPanicSend.java
+++ app/src/main/java/com/example/newsgsafety/ConfirmPanicSend.java
@@ -6,7 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class ConfirmPanicSend extends AppCompatActivity {
+public class
+
+
+ConfirmPanicSend extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
